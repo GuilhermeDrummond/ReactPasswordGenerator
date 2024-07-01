5524c51885ec8e226a6b60e851162f88a93d6d76
Guilherme Drummond <g_drummond@hotmail.com>
2024-07-01 16:36:31 -0300

    atualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Input from './hooks/Input'
 
 
 function App() {
-  const [password, setPassword] = useState("-")
+  const [password, setPassword] = useState("***")
   const [copyText, setCopyText] = useState("Copiar")
-  const [passwordSize, setPasswordSize] = useState(8)
+  const [customSize, setCustomSize] = useState(8)
+  const [showInput, setShowInput] = useState(false)
+
+  const passwordSize = showInput ? customSize : 8
 
   function gerador() {
     const caracters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
@@ -31,22 +35,25 @@ function App() {
   return (
     <div className="app">
       <h1>Gerador de Senha</h1>
-      <div className='tamanho'>
-        <h2 htmlFor="passwordSize">Tamanho:</h2>
-        <input 
-        type="number" 
-        id='passwordSize' min={1}
-        value={passwordSize}
-        onChange={(ev) => setPasswordSize(ev.target.value)}
-        />
+      <div>
+        <label htmlFor="showInput">Customizar tamanho:</label>
+        <input type="checkbox" id='showInput' value={showInput} onChange={() => setShowInput(currentState => !currentState)}/>
       </div>
+      {showInput ? (
+
+        <div className='tamanho'>
+        <h2 htmlFor="passwordSize">Tamanho:</h2>
+        <Input passwordSize={customSize} setPasswordSize={setCustomSize}/>
+        </div>
+
+      ) : null}
       
-      <div><label>Você selecionou uma senha com {passwordSize} caracteres:</label></div>
+      <div><label>Senha com {showInput ? passwordSize : 8} caracteres:</label></div>
       <div className='senha'>
       {password}
       </div>
       <button onClick={gerador}>Gerar</button>
-      <button onClick={copiar}>{copyText}</button>
+      <button onClick={copiar}>{copyText}</button>  
     </div>
   )
 }
